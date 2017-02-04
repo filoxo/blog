174eaa1022bacd528afb287b2a4c643754d1b06e
Carlos Filoteo <c.filoteo@live.com>
2017-02-03 21:36:02 -0700

    Update footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,15 +4,11 @@ import styles from "./index.css"
 
 const Footer = () => (
   <footer className={ styles.footer }>
-    { /* If you like Phenomic, this is a way to share the love ;) */ }
-    <p>
-      <a
-        href={ process.env.PHENOMIC_HOMEPAGE }
-        className={ styles.phenomicReference }
-      >
-        { "Website generated with " }
+    <p className={ styles.phenomicReference }>
+      { "Website generated with " }
+      <a href={ process.env.PHENOMIC_HOMEPAGE }>
         <span className={ styles.phenomicReferenceName }>
-          {  `<${ process.env.PHENOMIC_NAME} />` }
+          { `<${ process.env.PHENOMIC_NAME} />` }
         </span>
       </a>
     </p>
