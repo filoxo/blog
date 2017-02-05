62210f8c2920db59b5dad791a8edc1fee31cc1a5
Carlos Filoteo <c.filoteo@live.com>
2017-02-04 21:11:08 -0700

    Add LinkedIn to header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import Svg from "react-svg-inline"
 
 import twitterSvg from "../icons/iconmonstr-twitter-1.svg"
 import gitHubSvg from "../icons/iconmonstr-github-1.svg"
+import linkedinSvg from "../icons/iconmonstr-linkedin-1.svg"
 
 import styles from "./index.css"
 
@@ -26,17 +27,27 @@ const Header = (props, { metadata: { pkg } }) => (
             className={ styles.link }
           >
             <Svg svg={ twitterSvg } cleanup />
-            { "Twitter" }
+            <span className="sr-text">{ "Twitter" }</span>
           </a>
         }
         {
-          pkg.repository &&
+          pkg.github &&
           <a
-            href={ pkg.repository }
+            href={ pkg.github }
             className={ styles.link }
           >
             <Svg svg={ gitHubSvg } cleanup />
-            { "GitHub" }
+            <span className="sr-text">{ "GitHub" }</span>
+          </a>
+        }
+        {
+          pkg.linkedin &&
+          <a
+            href={ pkg.linkedin }
+            className={ styles.link }
+          >
+            <Svg svg={ linkedinSvg } cleanup />
+            <span className="sr-text">{ "LinkedIn" }</span>
           </a>
         }
       </div>
