dcfc52da90a9ae2531889e79636749f440c0a6a4
Carlos Filoteo <cfiloteo@ancestry.com>
2017-10-23 13:41:51 -0600

    Init using gatsby cli

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+const { name } = require('./package.json');
+
+module.exports = {
+  pathPrefix: process.env.CI ? `/${name}` : `/`,
+  siteMetadata: {
+    author: 'You!',
+    title: `Gatsby Default (Blog) Starter`,
+  },
+  plugins: [
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp'
+  ],
+}
