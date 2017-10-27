1cbc31aa5824573aa89626c366483a299fbe140c
Carlos Filoteo <cfiloteo@ancestry.com>
2017-10-27 16:11:49 -0600

    Update blog preview styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import '../css/typography.css'
+import '../css/main.css'
 
 export default class Template extends React.Component {
   static propTypes = {
