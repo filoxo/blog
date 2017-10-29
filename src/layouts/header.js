c203a2237d5993a9f15f7e77a55b553be7ef27fa
Carlos Filoteo <cfiloteo@ancestry.com>
2017-10-28 23:06:59 -0600

    Header

---
+++ src/layouts/header.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import SVGInline from 'react-svg-inline'
+import GithubIcon from 'react-icons/lib/fa/github-alt'
+import LinkedinIcon from 'react-icons/lib/fa/linkedin'
+import TwitterIcon from 'react-icons/lib/fa/twitter'
+
+import '../css/header.css'
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        background: `#fff`,
+        borderBottom: '2px solid rgba(160, 160, 160, 0.3)',
+        marginBottom: `1.45rem`
+      }}
+    >
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0.5rem 1.45rem`,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          flexWrap: 'wrap'
+        }}
+      >
+        <h1 style={{ margin: 0, fontSize: `1.5rem`, letterSpacing: 8 }}>
+          <Link
+            to="/"
+            style={{
+              color: '#333',
+              textDecoration: 'none',
+              textTransform: 'uppercase',
+              border: 'none'
+            }}
+          >
+            Filoblog
+          </Link>
+        </h1>
+        <ul className="menuIcons">
+          <li>
+            <a
+              href="https://twitter.com/cfiloteo12"
+              rel="noopener"
+              target="_blank"
+            >
+              <TwitterIcon size={20} />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/filoxo" rel="noopener" target="_blank">
+              <GithubIcon size={20} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/carlosfiloteo/"
+              rel="noopener"
+              target="_blank"
+            >
+              <LinkedinIcon size={20} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+import Header from './header'
 import '../css/main.css'
 
 export default class Template extends React.Component {
@@ -24,37 +25,12 @@ export default class Template extends React.Component {
             { name: 'keywords', content: 'sample, something' }
           ]}
         />
-        <div
-          style={{
-            background: `#333`,
-            marginBottom: `1.45rem`
-          }}
-        >
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `1rem 0.75rem`
-            }}
-          >
-            <h1 style={{ margin: 0, fontSize: `2rem` }}>
-              <Link
-                to="/"
-                style={{
-                  color: 'white',
-                  textDecoration: 'none'
-                }}
-              >
-                Filoblog
-              </Link>
-            </h1>
-          </div>
-        </div>
+        <Header />
         <div
           style={{
             margin: `0 auto`,
             maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            padding: `0 .5rem 1.45rem`,
             paddingTop: 0
           }}
         >
