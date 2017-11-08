9da0c2df019d17b77677da7a6dd8533d5a56d052
Carlos Filoteo <cfiloteo@ancestry.com>
2017-11-08 15:44:02 -0700

    Replace next/prev with back/top

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import BackIcon from 'react-icons/lib/fa/chevron-left'
-import ForwardIcon from 'react-icons/lib/fa/chevron-right'
+import BackIcon from 'react-icons/lib/io/ios-arrow-back'
+import UpIcon from 'react-icons/lib/io/ios-arrow-up'
 
 import Link from '../components/Link'
 import Tags from '../components/Tags'
 
 import '../css/blog-post.css'
+
+const scrollToTop = function scrollTo() {
+  var timeOut
+  function scrollToTop() {
+    if (
+      document.body.scrollTop != 0 ||
+      document.documentElement.scrollTop != 0
+    ) {
+      window.scrollBy(0, -50)
+      timeOut = setTimeout(scrollToTop, 10)
+    } else clearTimeout(timeOut)
+  }
+  scrollToTop()
+}
 
 export default function Template({ data, pathContext }) {
   const { markdownRemark: post } = data
@@ -15,8 +29,8 @@ export default function Template({ data, pathContext }) {
     <div className="blog-post-container">
       <Helmet title={`Gatsby Blog - ${post.frontmatter.title}`} />
       <div className="blog-post">
-        <h1 className="title">{post.frontmatter.title}</h1>
-        <h2 className="date">{post.frontmatter.date}</h2>
+        <h2 className="title">{post.frontmatter.title}</h2>
+        <div className="date">{post.frontmatter.date}</div>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: post.html }}
@@ -25,16 +39,12 @@ export default function Template({ data, pathContext }) {
           <Tags list={post.frontmatter.tags || []} />
         </div>
         <div className="navigation">
-          {prev && (
-            <Link className="link prev" to={prev.frontmatter.path}>
-              <BackIcon /> {prev.frontmatter.title}
-            </Link>
-          )}
-          {next && (
-            <Link className="link next" to={next.frontmatter.path}>
-              {next.frontmatter.title} <ForwardIcon />
-            </Link>
-          )}
+          <a className="link btn" href="/">
+            <BackIcon />&nbsp;Back
+          </a>
+          <button type="button" className="link btn" onClick={scrollToTop}>
+            Top&nbsp;<UpIcon />
+          </button>
         </div>
       </div>
     </div>
