e3f118c5294842ba96439cb41f82a2c6ef9822d9
Carlos Filoteo <c.filoteo12@gmail.com>
2019-01-28 15:33:47 -0700

    Replace grid with normal divs

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -6,11 +6,13 @@ import {linkBtn} from '../components/link.module.css'
 
 export default function PostPreview({ post }) {
   return (
-    <div className={styles.post} key={post.id}>
-      <h2 className={styles.title}>
-        <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
-      </h2>
-      <div className={styles.date}>{post.frontmatter.date}</div>
+    <div className={styles.post}>
+      <div className={styles.heading}>
+        <h2 className={styles.title}>
+          <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+        </h2>
+        <div className={styles.date}>{post.frontmatter.date}</div>
+      </div>
       <div className={styles.prev}>
         <p>{post.excerpt}</p>
         <Link className={linkBtn} to={post.fields.slug}>
