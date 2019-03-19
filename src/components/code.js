7a0be80815ab1ea85b97723b994ea25bd943ef4a
Carlos Filoteo <carlos.filoteo@podium.com>
2019-03-19 02:10:45 -0600

    Add syntax highlighting

---
+++ src/components/code.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/vsDarkPlus'
+
+export const Code = ({ codeString, language }) => {
+  return (
+    <Highlight
+      {...defaultProps}
+      code={codeString}
+      language={language}
+      theme={theme}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
