3862bb4f5f5d4ebfafa3f773b7c1687fc8ca5bea
Carlos Filoteo <carlos.filoteo@podium.com>
2019-03-19 03:02:10 -0600

    Add line highlighting

--- src/components/code.js
+++ src/components/code.js
@@ -2,7 +2,28 @@ import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/vsDarkPlus'
 
-export const Code = ({ codeString, language }) => {
+const RE = /{([\d,-]+)}/
+
+const calculateLinesToHighlight = (meta) => {
+  if (!RE.test(meta)) {
+    return () => false
+  } else {
+    const lineNumbers = RE.exec(meta)[1]
+      .split(',')
+      .map((v) => v.split('-').map((v) => parseInt(v, 10)))
+    return (index) => {
+      const lineNumber = index + 1
+      const inRange = lineNumbers.some(([start, end]) =>
+        end ? lineNumber >= start && lineNumber <= end : lineNumber === start
+      )
+      return inRange
+    }
+  }
+}
+
+export const Code = ({ codeString, language, metastring }) => {
+  const shouldHighlightLine = calculateLinesToHighlight(metastring)
+
   return (
     <Highlight
       {...defaultProps}
@@ -12,13 +33,19 @@ export const Code = ({ codeString, language }) => {
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
+          {tokens.map((line, i) => {
+            const lineProps = getLineProps({ line, key: i })
+            if (shouldHighlightLine(i)) {
+              lineProps.className = `${lineProps.className} highlight-line`
+            }
+            return (
+              <div {...lineProps}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            )
+          })}
         </pre>
       )}
     </Highlight>
