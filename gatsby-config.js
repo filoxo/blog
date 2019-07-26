f30070b1224f53a58eae69617788cb015b647b36
Carlos Filoteo <carlos.filoteo@podium.com>
2019-07-25 23:08:55 -0600

    Add gatsby-plugin-twitter

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-twitter`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
