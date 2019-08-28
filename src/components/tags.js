023799d2c03d78e4f2d9548bda23b853213a94ae
Carlos Filoteo <c.filoteo12@gmail.com>
2019-08-28 15:54:02 -0600

    Rename tags for case sensitivity

--- src/components/tags.js
+++ src/components/tags.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import styles from './tags.module.css'
-import {link} from '../components/link.module.css'
+import { link } from '../components/link.module.css'
 
 export default function Tags({ list = [] }) {
   return (
     <ul className={styles.tagList}>
-      {list.map(tag => (
+      {list.map((tag) => (
         <li key={tag}>
           <Link className={link} to={`/tags/${tag}`}>
             {tag}
