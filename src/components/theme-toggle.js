050238d711fea0ad645924ed139ab59288a34a54
Carlos Filoteo <c.filoteo12@gmail.com>
2019-11-30 22:57:22 -0700

    Add ThemeToggle

---
+++ src/components/theme-toggle.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import styles from './theme-toggle.module.css'
+
+const THEME = 'dark-theme-enabled'
+
+export default function ThemeToggle(props) {
+  const [darkModeEnabled, setDarkModeEnabled] = useState(() => {
+    return localStorage.getItem(THEME) === 'true'
+  })
+
+  useEffect(() => {
+    localStorage.setItem(THEME, darkModeEnabled)
+    document.body.classList.toggle('dark', darkModeEnabled)
+  }, [darkModeEnabled])
+
+  return (
+    <button
+      {...props}
+      type="button"
+      role="button"
+      className={styles.themeToggle}
+      aria-pressed={darkModeEnabled}
+      aria-label="Toggle dark theme"
+      onClick={() => setDarkModeEnabled(!darkModeEnabled)}
+    ></button>
+  )
+}
