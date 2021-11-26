d5de485f65e8d3027b8580df9f2117915f23a506
Carlos Filoteo <c.filoteo12@gmail.com>
2021-11-26 16:10:12 -0700

    Simplify Link

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Link as GatsbyLink } from 'gatsby'
-
-const baseClasses =
-  'text-red-600 hover:text-red-800 visited:text-red-900 no-underline'
+import cls from 'classnames'
 
 export const Link = (props) => (
   <GatsbyLink
     {...props}
-    className="text-red-600 hover:text-red-800 visited:text-red-900 no-underline"
+    className={cls(
+      'text-red-600 hover:text-red-700 no-underline transition-colors duration-300 ease-in-out',
+      props.className
+    )}
   />
-)
-export const LinkButton = (props) => (
-  <GatsbyLink {...props} className={`${baseClasses} border-2 py-1 px-3`} />
 )
