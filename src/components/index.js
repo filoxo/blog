81eaecb8b9a43c4e8f87a21d2de9e013c509e787
Carlos Filoteo <c.filoteo12@gmail.com>
2021-11-26 21:31:04 -0700

    Add new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,9 @@
 import { Pre } from './Pre'
 import { H1, H2, H3, H4, H5 } from './Headings'
 import { Ol, Ul } from './Lists'
+import { Aside } from './Aside'
+import { InlineLink } from './Link'
+import { Blockquote } from './Blockquote'
 // These shortcodes are what end up being available to the mdx renderer
 // so custom components and default element overrides should be added here
 export const components = {
@@ -12,4 +15,7 @@ export const components = {
   h5: H5,
   ol: Ol,
   ul: Ul,
+  Aside,
+  a: InlineLink,
+  blockquote: Blockquote,
 }
