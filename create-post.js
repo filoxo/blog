1277d6a363bf75a76f58e3de59a7eb2dd3b7c5ac
Carlos Filoteo <c.filoteo12@gmail.com>
2021-11-29 21:56:06 -0700

    Add create-post script

---
+++ create-post.js
@@ -0,0 +1,38 @@
+/*
+For posterity: this was the original npm script I used but eventually got annoyed at it.
+"post": "run(){ mkdir -p ./src/posts/$1 && touch ./src/posts/$1/index.mdx && echo \"---\ntitle: $1\ndate: 2020-MM-DD\ntags: []\n---\n\" >> ./src/posts/$1/index.mdx; }; run"
+*/
+
+const fs = require('fs')
+const path = require('path')
+const args = process.argv.slice(2)
+
+if (args.length < 1) {
+  console.log('Usage: node create-post.js "<title>"')
+  process.exit(1)
+}
+
+// https://www.geeksforgeeks.org/how-to-convert-a-string-into-kebab-case-using-javascript/
+const kebabCase = (str) =>
+  str
+    .replace(/([a-z])([A-Z])/g, '$1-$2') // replace "camelCase" to "camel-case"
+    .replace(/[\s_]+/g, '-') // Replace spaces with -
+    .toLowerCase()
+
+const title = args[0]
+const titleSlug = kebabCase(title)
+const todaysDate = new Date().toISOString().split('T')[0]
+
+// Create content
+const content = `---
+title: ${title}
+date: ${todaysDate}
+tags: []
+---
+
+`
+
+// Create post
+const dir = path.join(__dirname, 'src/posts', titleSlug)
+fs.mkdirSync(dir)
+fs.writeFileSync(path.join(dir, 'index.mdx'), content)
