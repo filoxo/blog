78ece937a3f8e5263fc9294000a5d8fc0cf7817b
Carlos Filoteo <c.filoteo12@gmail.com>
2022-02-10 14:36:37 -0700

    Add spacing

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -5,7 +5,7 @@ export const Aside = ({ info, warn, ...props }) => (
   <aside
     {...props}
     className={cls(
-      'pl-4 font-bold mt-2 border-l-4 text-black dark:text-white',
+      'pl-4 py-2 font-bold mt-2 border-l-4 text-black dark:text-white',
       {
         'border-red-600': !info && !warn,
         'border-blue-400': info,
