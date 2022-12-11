c4d8246feaa92cb29057cf6cdd05b855b5aa7a6a
Carlos Filoteo <c.filoteo12@gmail.com>
2022-12-10 22:53:43 -0700

    Gut it

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-import './src/styles/global.css'
-export { wrapRootElement } from './wrap-root-element'
--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {
-      // put stuff in tailwind.config.js, not here
-    },
-    autoprefixer: {},
-  },
-}
