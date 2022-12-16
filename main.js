86cdb3bf780ee478e46597ae8a560a45b8011f8d
Carlos Filoteo <c.filoteo12@gmail.com>
2022-12-15 21:10:35 -0700

    Remove DOMContentLoad as deferred script execute after dom parse

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-window.addEventListener('DOMContentLoaded', (_e) => {
-  window.theme = {
-    init: () => window.theme.set(window.theme.current()),
-    current: () => localStorage.getItem(THEME) === 'true',
-    set: (to) => {
-      document.documentElement.classList.toggle('dark', to)
-      const toggledState = document.documentElement.classList.contains('dark')
-      localStorage.setItem(THEME, toggledState)
-      return toggledState
-    },
-    toggle: () => window.theme.set(!window.theme.current()),
-  }
-  const THEME = 'theme:dark'
-  const toggle = document.getElementById('theme-toggle')
-  toggle.addEventListener('click', () => {
-    window.theme.toggle()
-  })
-  toggle.checked = window.theme.init()
+window.theme = {
+  init: () => window.theme.set(window.theme.current()),
+  current: () => localStorage.getItem(THEME) === 'true',
+  set: (to) => {
+    document.documentElement.classList.toggle('dark', to)
+    const toggledState = document.documentElement.classList.contains('dark')
+    localStorage.setItem(THEME, toggledState)
+    return toggledState
+  },
+  toggle: () => window.theme.set(!window.theme.current()),
+}
+const THEME = 'theme:dark'
+const toggle = document.getElementById('theme-toggle')
+toggle.addEventListener('click', () => {
+  window.theme.toggle()
 })
+toggle.checked = window.theme.init()
