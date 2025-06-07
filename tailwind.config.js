7e4fe94a9be2dd93b871333a66844a08d9e91e0b
Carlos Filoteo <c.filoteo12@gmail.com>
2025-06-06 20:18:24 -0600

    add output html as place where to look for classnames

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['src/**/*.{njk,md,html,css}'],
+  content: ['src/**/*.{njk,md,html,css}', './_site/**/*.html'],
   darkMode: 'class',
   plugins: [require('@tailwindcss/typography')],
 }
